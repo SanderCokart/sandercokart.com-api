4d57866b97e9575813f6202a619ae559bbb629b9
Sander Cokart <cokart32@gmail.com>
2023-04-23 13:44:44 +0200

    Added a helper function

--- app/helpers.php
+++ app/helpers.php
@@ -5,3 +5,10 @@ if (! function_exists('origin')) {
         return $_SERVER['HTTP_ORIGIN'] ?? null;
     }
 }
+
+if (! function_exists('random')) {
+    function random(...$args)
+    {
+        return $args[array_rand($args)];
+    }
+}
