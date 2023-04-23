b114f77bf53af7fc8f6b7ea3fb858c440fe84c90
Sander Cokart <cokart32@gmail.com>
2023-04-23 14:42:00 +0200

    Registered media collection for Course | Added traits

--- app/Models/Course.php
+++ app/Models/Course.php
@@ -2,20 +2,39 @@
 
 namespace App\Models;
 
+use App\Enums\MediaCollectionEnum;
+use App\Traits\Publishable;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\Relations\MorphOne;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Spatie\MediaLibrary\HasMedia;
+use Spatie\MediaLibrary\InteractsWithMedia;
 
-class Course extends Model
+class Course extends Model implements HasMedia
 {
-    use HasFactory;
+    use HasFactory, InteractsWithMedia, Publishable, SoftDeletes;
 
-    public $timestamps = false;
+    public function registerMediaCollections(): void
+    {
+        $this->addMediaCollection(MediaCollectionEnum::CourseBanners())
+            ->singleFile();
+    }
 
+    //<editor-fold desc="relationships">
     public function articles(): BelongsToMany
     {
         return $this->belongsToMany(Article::class)
             ->withPivot('order_column')
             ->using(ArticleCourse::class);
     }
+
+    public function banner(): MorphOne
+    {
+        return $this->media()
+            ->where('collection_name', MediaCollectionEnum::CourseBanners())
+            ->one();
+    }
+    //</editor-fold>
 }
