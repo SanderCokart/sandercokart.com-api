cba6bf06dded0f49c66afae08543b2ac5b0964cf
Sander Cokart <cokart32@gmail.com>
2023-04-23 14:35:35 +0200

    Edit course redirect to index

--- app/Filament/Resources/CourseResource/Pages/EditCourse.php
+++ app/Filament/Resources/CourseResource/Pages/EditCourse.php
@@ -16,4 +16,9 @@ class EditCourse extends EditRecord
             Actions\DeleteAction::make(),
         ];
     }
+
+    protected function getRedirectUrl(): ?string
+    {
+        return self::getResource()::getUrl('index');
+    }
 }
