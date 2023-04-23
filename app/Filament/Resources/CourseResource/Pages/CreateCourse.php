b850b8af942576f2e508a236f801bbc0dde3e6dd
Sander Cokart <cokart32@gmail.com>
2023-04-23 14:31:53 +0200

    Added redirect to index after course creation

--- app/Filament/Resources/CourseResource/Pages/CreateCourse.php
+++ app/Filament/Resources/CourseResource/Pages/CreateCourse.php
@@ -5,8 +5,14 @@ namespace App\Filament\Resources\CourseResource\Pages;
 use App\Filament\Resources\CourseResource;
 use Filament\Pages\Actions;
 use Filament\Resources\Pages\CreateRecord;
+use Illuminate\Database\Eloquent\Model;
 
 class CreateCourse extends CreateRecord
 {
     protected static string $resource = CourseResource::class;
+
+    protected function getRedirectUrl(): string
+    {
+        return $this->getResource()::getUrl('index');
+    }
 }
