c5b228244edad4d48d1efd81e388ad6d749d6e98
Sander Cokart <cokart32@gmail.com>
2023-08-06 01:40:43 +0200

    more conditionals for ArticleJsonResource.php

--- app/Http/Resources/ArticleJsonResource.php
+++ app/Http/Resources/ArticleJsonResource.php
@@ -16,16 +16,16 @@ class ArticleJsonResource extends JsonResource
     public function toArray(Request $request): array
     {
         return [
-            'id'           => $this->id,
-            'title'        => $this->title,
-            'slug'         => $this->slug,
-            'excerpt'      => $this->excerpt,
-            'banner'       => $this->whenLoaded('banner', fn() => $this->banner),
-            'type'         => $this->whenLoaded('type', fn() => $this->type),
-            'body'         => $this->whenHas('body'),
-            'created_at'   => $this->whenHas('created_at'),
-            'updated_at'   => $this->whenHas('updated_at'),
-            'published_at' => $this->whenHas('published_at'),
+            'id'                     => $this->whenHas('id'),
+            'title'                  => $this->whenHas('title'),
+            'slug'                   => $this->whenHas('slug'),
+            'excerpt'                => $this->whenHas('excerpt'),
+            'banner'                 => $this->whenLoaded('banner', fn() => $this->banner),
+            'type'                   => $this->whenLoaded('type', fn() => $this->type),
+            'body'                   => $this->whenHas('body'),
+            'created_at'             => $this->whenHas('created_at'),
+            'updated_at'             => $this->whenHas('updated_at'),
+            'published_at'           => $this->whenHas('published_at'),
             'estimated_reading_time' => $this->whenAppended('estimated_reading_time'),
         ];
     }
