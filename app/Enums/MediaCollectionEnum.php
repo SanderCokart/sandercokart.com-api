368b6cb539fc373f2f67f7720c6b35b6cbb6e0c9
Sander Cokart <cokart32@gmail.com>
2023-12-16 15:24:38 +0100

    Add CourseBanners to MediaCollectionEnum
    In this commit, a new enumeration case, CourseBanners, is added to the MediaCollectionEnum. This inclusion broadens the range of media collections that the app can handle, providing greater flexibility and use case coverage.

--- app/Enums/MediaCollectionEnum.php
+++ app/Enums/MediaCollectionEnum.php
@@ -6,10 +6,12 @@ use ArchTech\Enums\InvokableCases;
 
 /**
  * @method static string ArticleBanners()
+ * @method static string CourseBanners()
  */
 enum MediaCollectionEnum
 {
     use InvokableCases;
 
     case ArticleBanners;
+    case CourseBanners;
 }
