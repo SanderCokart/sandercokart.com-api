0070db327fec950f9b1b5a6ba31775977694caff
Sander Cokart <cokart32@gmail.com>
2023-12-16 15:25:13 +0100

    Refactor Course model and introduce media handling
    In this commit, the Course model has been refactored to use the HasFactory, SoftDeletes, and InteractsWithMedia traits. The model also now implements the HasMedia interface, enabling the manipulation of media files related to course instances.  A registerMediaCollections function was added and the model's casting has been updated for better date formatting.

--- app/Models/Course.php
+++ app/Models/Course.php
@@ -2,15 +2,28 @@
 
 namespace App\Models;
 
+use App\Enums\MediaCollectionEnum;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Spatie\MediaLibrary\HasMedia;
+use Spatie\MediaLibrary\InteractsWithMedia;
 
-class Course extends Model
+class Course extends Model implements HasMedia
 {
-    use HasFactory;
+    use HasFactory, InteractsWithMedia, SoftDeletes;
 
-    public $timestamps = false;
+    protected $casts = [
+        'published_at' => 'datetime:Y-m-d H:i:s',
+        'created_at' => 'datetime:Y-m-d H:i:s',
+        'updated_at' => 'datetime:Y-m-d H:i:s',
+    ];
+
+    public function registerMediaCollections(): void
+    {
+        $this->addMediaCollection(MediaCollectionEnum::CourseBanners());
+    }
 
     public function articles(): BelongsToMany
     {
@@ -18,4 +31,16 @@ class Course extends Model
             ->withPivot('order_column')
             ->using(ArticleCourse::class);
     }
+
+    //<editor-fold desc="scopes">
+    public function scopePublished($query)
+    {
+        return $query->whereNotNull('published_at');
+    }
+
+    public function scopeUnpublished($query)
+    {
+        return $query->whereNull('published_at');
+    }
+    //</editor-fold>
 }
