8536b8871fdd0901284e2ee5a26a5c1e3ef04074
Sander Cokart <cokart32@gmail.com>
2023-12-18 21:18:57 +0100

    Add CourseController with index action
    The CourseController class has been added and it features the index action. This action makes use of the QueryBuilder to facilitate complex queries on the Course model. Various parameters have been set up to customize the results returned including sorting, pagination and data fields.

---
+++ app/Http/Controllers/CourseController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Resources\CourseJsonCollection;
+use App\Models\Course;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Http\Request;
+use Spatie\QueryBuilder\QueryBuilder;
+
+class CourseController extends Controller
+{
+    public function index(Request $request): CourseJsonCollection
+    {
+        $perPage = $request->get('per_page', 10);
+
+        $courses = QueryBuilder::for(Course::class)
+            ->allowedFields('body')
+            ->with(['banner', 'type'])
+            ->allowedSorts('published_at', 'title')
+            ->defaultSort('-published_at')
+            ->addSelect('id', 'title', 'slug', 'published_at')
+            ->when($request->has('paginate'),
+                fn(Builder $query) => $query->paginate($perPage)->withQueryString(),
+                fn(Builder $query) => $query->when($request->has('cursorPaginate'),
+                    fn(Builder $query) => $query->cursorPaginate($perPage)->withQueryString(),
+                    fn(Builder $query) => $query->get()
+                )
+            );
+
+        return new CourseJsonCollection($courses);
+    }
+}
