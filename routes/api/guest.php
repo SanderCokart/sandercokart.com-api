2b19ce02904160b9147e79a9654f101034768ab8
Sander Cokart <cokart32@gmail.com>
2023-12-19 00:04:58 +0100

    Remove unnecessary comma in articles route
    A comma was unnecessarily place in the 'articles' route declaration in the api/guest.php file. This commit removes that, cleaning up the route declaration.

--- routes/api/guest.php
+++ routes/api/guest.php
@@ -9,7 +9,7 @@ use Illuminate\Support\Facades\Route;
 Route::post('/contact', ContactFormController::class)->middleware('throttle:2,10,contact-form')->name('contact');
 
 Route::get('/articles/paths', [ArticleController::class, 'paths'])->name('articles.paths');
-Route::get('/articles/{type:name?}', [ArticleController::class, 'index',])
+Route::get('/articles/{type:name?}', [ArticleController::class, 'index'])
     ->name('articles.index')
     ->whereIn('type', ArticleTypeEnum::names());
 Route::get('/articles/{type:name}/{article:slug}', [ArticleController::class, 'show'])->name('articles.show');
