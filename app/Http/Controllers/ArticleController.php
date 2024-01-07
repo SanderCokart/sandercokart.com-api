df00a331f40284eb1d5ca94ed3c2415c3330a4b1
Sander Cokart <cokart32@gmail.com>
2024-01-07 02:50:28 +0100

    Refactor ArticleController for better readability

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -17,26 +17,27 @@ class ArticleController extends Controller
     {
         $perPage = $request->get('per_page', 10);
 
-        $articles =
-            QueryBuilder::for(
-                Article::query()
-                    ->published()
-                    ->with([WithEnum::banner(), 'type'])
-            )
-                ->allowedSorts('published_at', 'title')
-                ->defaultSort('-published_at')
-                ->allowedFields('body')
-                ->addSelect('id', 'title', 'excerpt', 'slug', 'published_at', 'article_type_id')
-                ->when($type, fn(Builder $query) => $query->where('article_type_id', $type?->getId()))
-                ->when(
-                    $request->has('paginate'),
-                    fn(Builder $query) => $query->when(
-                        $request->has('cursor'),
-                        fn(Builder $query) => $query->cursorPaginate($perPage)->withQueryString(),
-                        fn(Builder $query) => $query->paginate($perPage)->withQueryString()
-                    ),
-                    fn(Builder $query) => $query->get()
-                );
+        $articles = QueryBuilder::for(
+            Article::query()
+                ->published()
+                ->with([WithEnum::banner(), 'type'])
+        )
+            ->allowedSorts('published_at', 'title')
+            ->defaultSort('-published_at')
+            ->allowedFields('body')
+            ->addSelect('id', 'title', 'excerpt', 'slug', 'published_at', 'article_type_id')
+            ->when($type, fn(Builder $query) => $query->where('article_type_id', $type?->getId()))
+            ->when(
+                $request->has('paginate'),
+                fn(Builder $query) => $query->when(
+                    $request->has('cursor'),
+                    fn(Builder $query) => $query->cursorPaginate($perPage)
+                        ->withQueryString(),
+                    fn(Builder $query) => $query->paginate($perPage)
+                        ->withQueryString()
+                ),
+                fn(Builder $query) => $query->get()
+            );
 
 
         return new ArticleJsonCollection($articles);
@@ -45,18 +46,23 @@ class ArticleController extends Controller
     public function show(string $type, string $slug): ArticleJsonResource
     {
         return new ArticleJsonResource(
-            Article::with([WithEnum::banner(), 'type'])->where('slug', $slug)->firstOrFail()
+            Article::with([WithEnum::banner(), 'type'])
+                ->where('slug', $slug)
+                ->firstOrFail()
         );
     }
 
     public function paths(): array
     {
-        return Article::with('type')->get()->map(fn(Article $article) => [
-            'params' => [
-                'type' => $article->type?->name,
-                'slug' => $article->slug,
-            ],
-        ]
-        )->toArray();
+        return Article::with('type')
+            ->get()
+            ->map(fn(Article $article) => [
+                'params' => [
+                    'type' => $article->type?->name,
+                    'slug' => $article->slug,
+                ],
+            ]
+            )
+            ->toArray();
     }
 }
