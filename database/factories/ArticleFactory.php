ee127f1e5c5af309b8a9e055fa89de29f73bb58b
Sander Cokart <cokart32@gmail.com>
2024-01-24 20:27:04 +0100

    Refactored ArticleFactory to dynamically generate image files.
    Introduced the usage of PHP's GD extension in the ArticleFactory to dynamically generate dummy images, replacing the previous static image file. This change improves the robustness of test cases by ensuring a unique image for every created Article instance. The images are created with random colors, dimensions, and are named based on the Article's title, further reflecting realistic scenarios.

--- database/factories/ArticleFactory.php
+++ database/factories/ArticleFactory.php
@@ -22,15 +22,15 @@ class ArticleFactory extends Factory
     public function definition(): array
     {
         return [
-            'title'           => $this->faker->realText(50),
-            'excerpt'         => $this->faker->realText(120),
-            'slug'            => $this->faker->slug,
-            'body'            => $this->faker->realText(1000),
+            'title' => $this->faker->realText(50),
+            'excerpt' => $this->faker->realText(120),
+            'slug' => $this->faker->slug,
+            'body' => $this->faker->realText(1000),
             'article_type_id' => ArticleTypeEnum::random()->getId(),
             //dates between now and 30 days ago
-            'created_at'      => $created_at = now()->subDays(random_int(1, 30)),
-            'updated_at'      => $updated_at = $this->faker->dateTimeBetween($created_at, 'now'),
-            'published_at'    => $this->faker->dateTimeBetween($updated_at, 'now'),
+            'created_at' => $created_at = now()->subDays(random_int(1, 30)),
+            'updated_at' => $updated_at = $this->faker->dateTimeBetween($created_at, 'now'),
+            'published_at' => $this->faker->dateTimeBetween($updated_at, 'now'),
         ];
     }
 
@@ -71,12 +71,36 @@ class ArticleFactory extends Factory
     public function configure(): self
     {
         return $this->afterCreating(function (Article $article) {
-            $article->addMedia(UploadedFile::fake()->image('banner.jpg', 300, 200))
+            $file = $this->createImageUploadedFile(300, 200, $article->title);
+            $article->addMedia($file)
                 ->toMediaCollection(
                     MediaCollectionEnum::ArticleBanners(),
                     DiskEnum::public()
                 );
         });
+    }
+
+    private function createImageUploadedFile(string $width, string $height, string $title): UploadedFile
+    {
+        $colors = collect(['FF0000', '00FF00', '0000FF', 'FFFF00', '00FFFF', 'FF00FF']);
+        $color = $colors->random();
+
+        $image = imagecreate($width, $height);
+        $red = hexdec(substr($color, 0, 2));
+        $green = hexdec(substr($color, 2, 2));
+        $blue = hexdec(substr($color, 4, 2));
+        $background = imagecolorallocate($image, $red, $green, $blue);
+        $textcolor = imagecolorallocate($image, 0,0,0);
+
+
+        //text in the middle
+        imagestring($image, 16, 50, 50, "#{$color}", $textcolor);
+        imagestring($image, 16, 50, 75, $title, $textcolor);
+
+
+        imagejpeg($image, $filename = tempnam(sys_get_temp_dir(), 'article_banner_'), 100);
+        imagedestroy($image);
+        return new UploadedFile($filename, 'article_banner.jpg', 'image/jpeg', null, true);
     }
 
 }
