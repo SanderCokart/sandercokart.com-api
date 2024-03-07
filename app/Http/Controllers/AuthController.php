8dc796ad0294045173c827f09fd11e3d8d9300a8
Sander Cokart <cokart32@gmail.com>
2024-03-07 16:00:29 +0100

    wip

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+
+class AuthController extends Controller
+{
+    public function register(Request $request): \Illuminate\Http\JsonResponse
+    {
+        $fields = $request->validate([
+            'name' => 'required|string',
+            'email' => 'required|email|unique:users,email',
+            'password' => 'required',
+        ]);
+
+        $user = User::create([
+            'name' => $fields['name'],
+            'email' => $fields['email'],
+            'password' => Hash::make($fields['password']),
+        ]);
+
+        $response = [
+            'success' => true,
+            'message' => "Registration successful."
+        ];
+        return response()->json($response, 201);
+    }
+
+
+
+
+
+}
